7dc7bd82a426cebaf05df2c48c3cf8ea57736153
TomNelsonTembo <neltembo2002gmail.com>
2024-04-21 23:50:31 +0200

    metrics metrics metrics!!!

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from prometheus_client.core import Summary, Counter, Histogram, Gauge
+from prometheus_client import CollectorRegistry
 from app.Model.model import Predict_pipeline
 from app.Model.model import __version__ as ModelVersion
+import time
 
 
 
@@ -9,6 +12,9 @@ from app.Model.model import __version__ as ModelVersion
 
 app = FastAPI()
 
+graphs = {}
+graphs['c'] = Counter('l_m_total_requests', 'Total numnber of language model requests' )
+graphs['h'] = Histogram('l_m_request_latency_seconds', 'Histogram from Inference time')
 
 class TextIn(BaseModel):
     text: str
@@ -25,5 +31,17 @@ def home():
 
 @app.post("/predict", response_model=PredictionOut)
 def predict(payload: TextIn):
+    start = time.time()
+    graphs['s'].inc()
     language = Predict_pipeline(payload.text)
+    end = time.time()
+    graphs['h'].observe(end - start)
     return {"language": language}
+
+@app.get("/metrics")
+def requests_count():
+    res = []
+    for k,v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+    return Reponse(res, mimetype="text/plain")
+
